3f6edad7ca3b8cab4cf7733c269afea9631aa51f
Favero Pereira <phfaverop@gmail.com>
2018-12-02 15:05:17 +0100

    Singleton Hive implemented

--- src/favero/moreira/sma/termoreg/Hive.java
+++ src/favero/moreira/sma/termoreg/Hive.java
@@ -1,5 +1,38 @@
 package favero.moreira.sma.termoreg;
 
 public class Hive {
+	
+	protected double temp;
+	
+
+
+	//create an object of SingleObject
+	   private static Hive instance = new Hive();
+	   
+
+	   //make the constructor private so that this class cannot be
+	   //instantiated
+	   private Hive(){
+		   
+		   
+		   
+	   }
+
+	   //Get the only object available
+	   public static Hive getInstance(){
+	      return instance;
+	   }
+
+		public double getTemp() {
+			return temp;
+		}
+
+		public void setTemp(double temp) {
+			this.temp = temp;
+		}
+	   
+	   public void showMessage(){
+	      System.out.println("I'm a hive with a lot of honey :) !");
+	   }
 
 }
--- src/favero/moreira/sma/termoreg/Main.java
+++ src/favero/moreira/sma/termoreg/Main.java
@@ -2,4 +2,16 @@ package favero.moreira.sma.termoreg;
 
 public class Main {
 
+	public static void main(String[] args) {
+
+		// Get the only object available
+		Hive hive = Hive.getInstance();
+
+		// show the message
+		hive.showMessage();
+		hive.setTemp(35.0);
+		
+		System.out.println("Hive temperature: " + hive.getTemp());
+	}
+
 }
