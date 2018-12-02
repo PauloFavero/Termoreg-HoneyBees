09d02ebb2cc99ef2f9eef2558726cf144987c396
Favero Pereira <phfaverop@gmail.com>
2018-12-02 15:13:04 +0100

    Bee class add

---
+++ src/favero/moreira/sma/termoreg/Bee.java
@@ -0,0 +1,39 @@
+package favero.moreira.sma.termoreg;
+
+public class Bee {
+	
+	private double dThreshold;
+	private double dPrecision;
+	private double dSupLim;
+	private double dInfLim;
+	
+	public double getdThreshold() {
+		return dThreshold;
+	}
+	public void setdThreshold(double dThreshold) {
+		this.dThreshold = dThreshold;
+	}
+	public double getdPrecision() {
+		return dPrecision;
+	}
+	public void setdPrecision(double dPrecision) {
+		this.dPrecision = dPrecision;
+	}
+	public double getdSupLim() {
+		return dSupLim;
+	}
+	public void setdSupLim(double dSupLim) {
+		this.dSupLim = dSupLim;
+	}
+	public double getdInfLim() {
+		return dInfLim;
+	}
+	public void setdInfLim(double dInfLim) {
+		this.dInfLim = dInfLim;
+	}
+
+	
+	
+	
+
+}
